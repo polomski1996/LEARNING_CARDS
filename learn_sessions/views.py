fa7525b96cede5d6ae734d5767e054c0ca2dd865
polomski1996 <polomskimaciej5@gmail.com>
2025-12-27 11:15:09 +0100

    removed unnecessary import from lear_sesion views.py

--- learn_sessions/views.py
+++ learn_sessions/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Learn_session
 from sets.models import Set, Card
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
 
